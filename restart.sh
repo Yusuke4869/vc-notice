138ca2444515e43dc7c33aae111b5a988c6d8e53
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-01-05 20:36:01 +0900

    🔨 update restart script

--- restart.sh
+++ restart.sh
@@ -1,10 +1,13 @@
+PATH=$PATH:$HOME/.volta/bin
+
 git checkout .
 git pull origin main
+
 yarn install
-yarn build
+yarn run build
 
 for PID in $(ps -e -o pid,cmd | grep yarn | grep start-vc-notice | awk '{print $1}'); do
   kill $PID
 done
 
-nohup yarn start-vc-notice &
+nohup yarn run start-vc-notice &
