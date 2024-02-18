681bace4f1dd2c7d4c13184167f0e838cca6cb52
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-02-19 00:42:51 +0900

    ⚡️ improvement and rename index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
 export * from "./join";
+export * from "./member";
+export * from "./mention";
+export * from "./slashCommand";
 export * from "./vc";
 export * from "./webhook";
-export * from "./member";
---
+++ src/services/slashCommand.ts
@@ -0,0 +1,13 @@
+import type { Client, CommandInteraction } from "discord.js";
+
+import { about, help, ping, set } from "../commands";
+
+export const slashCommand = async (client: Client, interaction: CommandInteraction) => {
+  const { commandName } = interaction;
+
+  if (commandName === "help") await help(interaction);
+  else if (commandName === "ping") await ping(client, interaction);
+  else if (commandName === "about") await about(interaction);
+  else if (commandName === "set") await set(client, interaction);
+  else await help(interaction);
+};
