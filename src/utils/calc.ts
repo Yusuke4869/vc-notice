415741d79d422f758a9124b8bb008f0133e58a14
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-04-03 08:54:23 +0900

    ⚡️ improvement of utility functions

--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -1,5 +1,6 @@
 /**
  * 経過時間を文字列の形式に変換
+ *
  * @param passedTime 経過時間（秒）
  * @returns hh:mm:ss形式の文字列
  */
@@ -8,8 +9,8 @@ export const calcTime = (passedTime: number) => {
   const minutes = Math.floor((passedTime - hours * 60 * 60) / 60);
   const seconds = passedTime - hours * 60 * 60 - minutes * 60;
 
-  const strHours = hours < 100 ? hours.toString().padStart(2, "0") : hours.toString();
-  const strMinutes = minutes.toString().padStart(2, "0");
-  const strSeconds = seconds.toString().padStart(2, "0");
-  return `${strHours}:${strMinutes}:${strSeconds}`;
+  const h = hours < 100 ? hours.toString().padStart(2, "0") : hours.toString();
+  const m = minutes.toString().padStart(2, "0");
+  const s = seconds.toString().padStart(2, "0");
+  return `${h}:${m}:${s}`;
 };
--- src/utils/embed.ts
+++ src/utils/embed.ts
@@ -1,12 +1,20 @@
 import { ColorResolvable, EmbedBuilder, Locale } from "discord.js";
 
-import type { EmbedContents } from "../types";
+import type { EmbedContents, TLanguage } from "../types";
 import { locale2language } from "./language";
 
 export const defaultColor: ColorResolvable = "#faa61a";
 
-export const buildEmbed = (contents: EmbedContents, locale: Locale) => {
-  const lang = locale2language(locale);
+/**
+ * Embedを生成
+ *
+ * @param contents コンテンツ
+ * @param locale ロケール（言語を指定するとこの値は無視されます）
+ * @param language 言語
+ * @returns
+ */
+export const buildEmbed = (contents: EmbedContents, locale: Locale, language?: TLanguage): EmbedBuilder => {
+  const lang = language ?? locale2language(locale);
 
   const res = new EmbedBuilder()
     .setTitle(contents[lang].title)
