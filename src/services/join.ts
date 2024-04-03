4867ffe27f1b118cc63afc09b7c9ff977b5031c9
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-04-03 09:00:51 +0900

    ⚡️ improvement of join events

--- src/services/join.ts
+++ src/services/join.ts
@@ -1,25 +1,11 @@
-import { EmbedBuilder } from "discord.js";
-import type { Client, Guild } from "discord.js";
+import type { Guild } from "discord.js";
 
-const embed = (client: Client) => {
-  const res = new EmbedBuilder()
-    .setAuthor({ name: client.user?.username ?? "VC Notice", iconURL: client.user?.displayAvatarURL() })
-    .setTitle("Hi, there👋")
-    .setDescription("Thanks for inviting me")
-    .setColor("#FAA61A")
-    .addFields([
-      {
-        name: "How to use",
-        value: "Type /help",
-      },
-    ])
-    .setFooter({ text: "VC Notice" });
-  return res;
-};
+import { joinEmbed } from "../embed/join";
+import { buildEmbed } from "../utils";
 
-export const join = async (client: Client, guild: Guild) => {
+export const join = async (guild: Guild) => {
   try {
-    await guild.systemChannel?.send({ embeds: [embed(client)] });
+    await guild.systemChannel?.send({ embeds: [buildEmbed(joinEmbed(), guild.preferredLocale)] });
   } catch (e) {
     console.error(e);
   }
