244b7e39cbbe4a7badd015f29d1c47b29399cba5
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-10-03 11:54:30 +0900

    feat: add noManageWebhooksPermissionErrorEmbed

--- src/embed/commands/set/errors.ts
+++ src/embed/commands/set/errors.ts
@@ -28,3 +28,32 @@ export const setNotFoundInteractionChannelErrorEmbed = (): EmbedContents => ({
     },
   },
 });
+
+export const noManageWebhooksPermissionErrorEmbed = (): EmbedContents => ({
+  en: {
+    title: "An error occurred!",
+    color: "#ff0000",
+    fields: [
+      {
+        name: "Permission required",
+        value: 'You need the "Manage Webhooks" permission in VC Notice',
+      },
+    ],
+    footer: {
+      text: "Please grant permission and try again",
+    },
+  },
+  ja: {
+    title: "エラーが発生しました",
+    color: "#ff0000",
+    fields: [
+      {
+        name: "権限が必要です",
+        value: "VC Notice に「ウェブフックの管理」の権限が必要です",
+      },
+    ],
+    footer: {
+      text: "権限を付与してからもう一度お試しください",
+    },
+  },
+});
