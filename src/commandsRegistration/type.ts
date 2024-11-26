2eb7be9ae38c3543c1661559d59384f4398da5ec
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-11-26 11:19:30 +0900

    fix: fix linter errors

--- src/commandsRegistration/type.ts
+++ src/commandsRegistration/type.ts
@@ -2,43 +2,31 @@ import type { TLanguage } from "../types/language";
 
 interface SlashCommandSubcommandConfig {
   name: string;
-  description: {
-    [key in TLanguage]: string;
-  };
+  description: Record<TLanguage, string>;
   stringOptions?: {
     name: string;
-    description: {
-      [key in TLanguage]: string;
-    };
+    description: Record<TLanguage, string>;
     required: boolean;
     choices: {
-      name: {
-        [key in TLanguage]: string;
-      };
+      name: Record<TLanguage, string>;
       value: string;
     }[];
   }[];
   booleanOptions?: {
     name: string;
-    description: {
-      [key in TLanguage]: string;
-    };
+    description: Record<TLanguage, string>;
     required: boolean;
   }[];
   roleMentionOptions?: {
     name: string;
-    description: {
-      [key in TLanguage]: string;
-    };
+    description: Record<TLanguage, string>;
     required: boolean;
   }[];
 }
 
 export interface SlashCommandConfig {
   name: string;
-  description: {
-    [key in TLanguage]: string;
-  };
+  description: Record<TLanguage, string>;
   allowDM: boolean;
   permissions?: bigint | null;
   subCommands?: SlashCommandSubcommandConfig[];
--- src/types/embed.ts
+++ src/types/embed.ts
@@ -2,8 +2,9 @@ import type { TLanguage } from "./language";
 import type { voiceChannelActivityType } from "./vc";
 import type { APIEmbedField, ColorResolvable, EmbedAuthorOptions, EmbedFooterOptions } from "discord.js";
 
-export type EmbedContents = {
-  [key in TLanguage]: {
+export type EmbedContents = Record<
+  TLanguage,
+  {
     title: string | null;
     description?: string | null;
     author?: EmbedAuthorOptions | null;
@@ -14,22 +15,17 @@ export type EmbedContents = {
     footer?: EmbedFooterOptions | null;
     timestamp?: number | Date | null | undefined;
     fields?: APIEmbedField[] | null;
-  };
-};
+  }
+>;
 
-export type NoticeEmbedText = {
-  [key in TLanguage]: {
-    title: {
-      [key in voiceChannelActivityType]: string;
-    };
-    description: {
-      [key in voiceChannelActivityType]: string;
-    };
+export type NoticeEmbedText = Record<
+  TLanguage,
+  {
+    title: Record<voiceChannelActivityType, string>;
+    description: Record<voiceChannelActivityType, string>;
     footer: {
       callTime: string;
     };
-    color: {
-      [key in voiceChannelActivityType]: ColorResolvable;
-    };
-  };
-};
+    color: Record<voiceChannelActivityType, ColorResolvable>;
+  }
+>;
